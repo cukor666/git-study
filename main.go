043988003dd85e31999e6c3e85f5a3a183b36794
Cukor <2450808861@qq.com>
2024-05-16 21:03:34 +0800

    去掉一些没有使用的包

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"git-study/dao"
-	mymath "git-study/my_math"
-	mytest "git-study/test"
 )
 
 func main() {
 	fmt.Println("Cukor是个大帅哥！！！")
+	fmt.Println("Cukor is a handsome guy!!!")
 }
