1f8da3caa91aa4d93d7b12d9293f183717cedb60
Cukor <2450808861@qq.com>
2024-05-16 13:52:39 +0800

    添加了add函数

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func add(a, b int) int {
+	return a + b
+}
+
 func main() {
 	fmt.Println("Hello, world!")
+	a := 13
+	b := 45
+	c := add(a, b)
+	fmt.Println("a + b = ", c)
 }
