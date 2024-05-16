2eb70d802b2a78c706a2604b40d214d7201e0c06
Cukor <2450808861@qq.com>
2024-05-16 18:36:04 +0800

    添加插入用户功能

--- dao/userdao.go
+++ dao/userdao.go
@@ -1,7 +1,15 @@
 package dao
 
+import "fmt"
+
 type UserDao struct{}
 
 func (u *UserDao) GetUser(id int) string {
+	fmt.Println("根据ID获取用户信息")
 	return "UserDao.GetUser"
+}
+
+func (u *UserDao) InsertUser(name string, age int) int {
+	fmt.Println("插入用户信息")
+	return 1
 }
