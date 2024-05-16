6b802204e2603966721c27ec3f97a4f9fcfac8a0
Cukor <96514370+cukor666@users.noreply.github.com>
2024-05-16 20:59:15 +0800

    远程仓库修改main.go

--- main.go
+++ main.go
@@ -8,26 +8,5 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, world!")
-
-	a := 13
-	b := 45
-	c := mymath.Add(a, b)
-	fmt.Println("a + b = ", c)
-	a = 10
-	b = 20
-
-	c = mymath.Mult(a, b)
-	fmt.Println("a * b = ", c)
-
-	c = mymath.Sub(a, b)
-	fmt.Println("a - b = ", c)
-	a = 5
-	b = 2
-	c = mymath.Div(a, b)
-	fmt.Println("a / b = ", c)
-	mytest.Test()
-	userDao := dao.UserDao{}
-	s := userDao.GetUser(3)
-	fmt.Println(s)
+	fmt.Println("Cukor是个大帅哥！！！")
 }
